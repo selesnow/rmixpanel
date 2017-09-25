76390720be7804d431ccdf3a2a5f478e9cc6c091
Alexey Seleznev <selesnow@mail.ru>
2017-09-25 13:15:21 +0300

    Update MP.getEvents.R

--- R/MP.getEvents.R
+++ R/MP.getEvents.R
@@ -5,27 +5,27 @@ MP.getEvents <- function(api_secret = NULL,
                          interval = NULL,
                          from_date = NULL,
                          to_date = NULL){
-  #Ïðîâåðêà àãðóìåíòîâ
+  #ÃÃ°Ã®Ã¢Ã¥Ã°ÃªÃ  Ã Ã£Ã°Ã³Ã¬Ã¥Ã­Ã²Ã®Ã¢
   if(is.null(interva) & is.null(from_date) & is.null(to_date)){
     from_date <- Sys.Date() - 10
     to_date = Sys.Date()
     }
   
-  #Ôîðìèðîâàíèå çàïðîñà
+  #Ã”Ã®Ã°Ã¬Ã¨Ã°Ã®Ã¢Ã Ã­Ã¨Ã¥ Ã§Ã Ã¯Ã°Ã®Ã±Ã 
   query_string <- paste0('https://',api_secret,'@mixpanel.com/api/2.0/events/?',
                          'event=',toJSON(event),
                          '&type=',type,
                          '&unit=',unit,
-                         ifelse(is.null(interval),'',paste0('&interval=',interva,'&')),
+                         ifelse(is.null(interval),'',paste0('&interval=',interval,'&')),
                          ifelse(is.null(from_date),'',paste0('&from_date=',from_date)),
                          ifelse(is.null(to_date),'',paste0('&to_date=',to_date)),
                          '&format=csv')
   
-  #Îòïðàâêà çàïðîñà ê API
+  #ÃŽÃ²Ã¯Ã°Ã Ã¢ÃªÃ  Ã§Ã Ã¯Ã°Ã®Ã±Ã  Ãª API
   api_answer <- GET(query_string)
   stop_for_status(api_answer)
   mixpaneleventdata <- content(api_answer, "parsed", "text/csv")
   
-  #Âîçâðàùàåì îòâåò
+  #Ã‚Ã®Ã§Ã¢Ã°Ã Ã¹Ã Ã¥Ã¬ Ã®Ã²Ã¢Ã¥Ã²
   return(mixpaneleventdata)
 }
